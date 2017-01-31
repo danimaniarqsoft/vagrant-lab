e8b2e205a41d18aa9bd340e97b77b9dd7baa42ba
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-01-31 00:19:51 -0600

    refactoring the files

--- vm-centos7/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-printf "\n" >> /home/vagrant/.bashrc
-echo 'export PS1="\[\e[01;34m\]\u\[\e[0m\]\[\e[01;37m\]:\H:\w\[\e[0m\]\[\e[00;37m\]\n\\$ \[\e[0m\]"' >> /home/vagrant/.bashrc
-printf "\n" >> /home/vagrant/.bashrc
-
