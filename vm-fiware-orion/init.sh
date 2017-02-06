89dd4ae494430a3c3c7982fb81d3f2900a2d174a
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-02-06 11:36:27 -0600

    se agregan al usuario vagrant al grupo de docker

--- vm-fiware-orion/init.sh
+++ vm-fiware-orion/init.sh
@@ -16,7 +16,7 @@ curl -fsSL https://get.docker.com/ | sh
 sudo systemctl start docker
 sudo systemctl status docker
 sudo systemctl enable docker
-sudo usermod -aG docker $(whoami)
+sudo usermod -aG docker vagrant
 
 # install dockers-composed
 
