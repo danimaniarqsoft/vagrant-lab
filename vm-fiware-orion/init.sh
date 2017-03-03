78099518593ac7c21578e0719527667314c4297a
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-03-02 21:54:58 -0600

    creating sdk example

--- vm-fiware-orion/init.sh
+++ vm-fiware-orion/init.sh
@@ -10,11 +10,11 @@ printf "\n" >> /home/vagrant/.bashrc
 
 sudo yum -y check-update
 sudo yum -y install vim
+sudo yum -y install emacs-nox git nmap net-tools
 curl -fsSL https://get.docker.com/ | sh
 curl https://gist.githubusercontent.com/danimaniarqsoft/177b6c8cb579f0cac87b8d13d74e886c/raw/cdbdd584defb996b570ec068388405ec2f017741/docker-compose.yml > docker-compose.yml
-sudo systemctl start docker
-sudo systemctl status docker
 sudo systemctl enable docker
+sudo systemctl start docker
 sudo usermod -aG docker vagrant
 
 # install dockers-composed
@@ -26,3 +26,9 @@ sudo pip install backports.ssl_match_hostname --upgrade
 sudo pip install docker-compose
 sudo yum -y upgrade python*
 
+sudo yum clean all
+
+# Por bug de vagrant se forza el inicio de la interface de red
+[ $(ifconfig eth1 | grep inet | wc -l) = 0 ] && ifup eth1
+sudo chmod +x /etc/rc.d/rc.local
+sudo echo "[ $(ifconfig eth1 | grep inet | wc -l) = 0 ]" >> /etc/rc.d/rc.local
