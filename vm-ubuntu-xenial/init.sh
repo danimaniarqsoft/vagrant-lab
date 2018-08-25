e8f02ab6cbd2f80189eecd3daa5fb3d9a626f24b
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-08-24 19:06:01 -0500

    uploading files

--- vm-ubuntu-xenial/init.sh
+++ vm-ubuntu-xenial/init.sh
@@ -2,9 +2,11 @@
 
 # installa java
 
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install -y oracle-java8-installer
+# sudo apt-get install -y python-software-properties debconf-utils
+# sudo add-apt-repository -y ppa:webupd8team/java
+# sudo apt-get update
+# sudo apt-get install -y oracle-java8-installer
+
 # install node
 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
@@ -13,9 +15,12 @@ sudo apt-get install -y nodejs
 # Install yarn
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
+echo 'export PATH=$PATH:"/home/ubuntu/.yarn/bin"' >> /home/ubuntu/.bashrc
 
+source /home/ubuntu/.bashrc
 yarn global add yo
 yarn global add generator-jhipster
-echo 'export PATH=$PATH:"/home/ubuntu/.yarn/bin"' >> /home/ubuntu/.bashrc
+
 
