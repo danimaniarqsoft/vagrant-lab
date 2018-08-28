86a999b21217fc5be28d87ef666e9bbcf008fa70
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-08-27 19:02:54 -0500

    udpating files

--- vm-ubuntu-xenial/init.sh
+++ vm-ubuntu-xenial/init.sh
@@ -25,10 +25,36 @@ sudo apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
-echo 'export PATH=$PATH:"/home/ubuntu/.yarn/bin"' >> /home/ubuntu/.bashrc
+echo 'export PATH=$PATH:"/home/ubuntu/.yarn/bin":"/home/ubuntu/consul"' >> /home/ubuntu/.bashrc
 
 source /home/ubuntu/.bashrc
 yarn global add yo
 yarn global add generator-jhipster
 
+# Install Docker
 
+#sudo apt-get install --no-install-recommends apt-transport-https curl software-properties-common
+#curl -fsSL 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | sudo apt-key add -
+#sudo add-apt-repository "deb https://packages.docker.com/1.12/apt/repo/ ubuntu-$(lsb_release -cs) main"
+#sudo apt-get update
+#sudo apt-get -y install docker-engine
+#sudo usermod -a -G docker $USER
+## Install Docker Compose
+
+#sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+
+# Install Mongodb
+
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+# sudo apt-get update
+# sudo apt-get install -y mongodb-org
+
+# Tools
+
+sudo apt install unzip
+
+# Download consul
+#mkdir consult
+#curl -o consul/consul https://releases.hashicorp.com/consul/1.2.2/consul_1.2.2_linux_amd64.zip
