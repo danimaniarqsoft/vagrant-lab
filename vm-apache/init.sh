8f6d4ab0ebff7a22be9a9c5bdcc1625598de0ebe
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-12-17 22:47:29 -0600

    rename vm

--- vm-apache/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-printf "\n" >> /home/vagrant/.bashrc
-echo 'export PS1="\[\e[01;34m\]linux-apache\[\e[0m\]\[\e[01;37m\]:\w\[\e[0m\]\[\e[00;37m\]\n\\$ \[\e[0m\]"' >> /home/vagrant/.bashrc
-printf "\n" >> /home/vagrant/.bashrc
-
