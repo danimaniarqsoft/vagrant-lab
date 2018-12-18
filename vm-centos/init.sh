4190424abac0acc99e04869914b6aefc50ac2964
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-12-18 01:07:18 -0600

    adding

--- vm-centos/init.sh
+++ vm-centos/init.sh
@@ -4,3 +4,9 @@ printf "\n" >> /home/vagrant/.bashrc
 echo 'export PS1="\[\e[01;34m\]linux-centos\[\e[0m\]\[\e[01;37m\]:\w\[\e[0m\]\[\e[00;37m\]\n\\$ \[\e[0m\]"' >> /home/vagrant/.bashrc
 printf "\n" >> /home/vagrant/.bashrc
 
+# Install Ngix
+
+sudo yum -y install epel-release
+sudo yum -y install nginx
+sudo systemctl start nginx
+
